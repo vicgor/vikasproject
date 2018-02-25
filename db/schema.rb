4cb7a271327c3a8b523d9a2c859389765b58e617
Yanina23 <y.mosilevich@gmail.com>
2018-02-25 13:00:49 +0300

    Style changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401111413) do
+ActiveRecord::Schema.define(version: 20170408094415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170401111413) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "text"
   end
 
   create_table "testimonials", force: :cascade do |t|
